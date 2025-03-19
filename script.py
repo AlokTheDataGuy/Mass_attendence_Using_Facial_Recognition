5f041d9bf19474792e85cd611b944102a8e12ef6
Alok Deep <126234950+AlokTheDataGuy@users.noreply.github.com>
2025-03-19 06:24:11 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,9 @@
+import numpy as np
+import scipy
+import sklearn
+import cv2
+
+print("NumPy version:", np.__version__)
+print("SciPy version:", scipy.__version__)
+print("scikit-learn version:", sklearn.__version__)
+print("OpenCV version:", cv2.__version__)
